1febe4c394065fdba1ec318b0e6970fe844b3282
Oskar Gauffin <oskar.gauffin@hotmail.com>
2022-12-22 18:40:56 +0000

    color function added

---
+++ color_themes.R
@@ -0,0 +1,17 @@
+'# A function to return one of two custom colours, for use in plotting.
+'# @params colour Must be either blue or green.
+
+
+custom_colours <- function(colour = c("blue", "green")){
+
+  colour ="green"
+  checkmate::check_string(colour)
+  checkmate::checkSubset(colour, c("green","blue"))
+
+  umc_blue =
+  umc_green =
+
+  return_colour <- ifelse(colour == "blue", umc_blue, umc_green)
+
+  return(return_colour)
+}
