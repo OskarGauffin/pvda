901627eef6ce7618695d4549c82ae076b6970d4c
Oskar Gauffin <7349924+OskarGauffin@users.noreply.github.com>
2023-01-09 20:34:32 +0100

    More documentation

--- R/ic.R
+++ R/ic.R
@@ -1,12 +1,12 @@
 #' @title Confidence intervals for Reporting Odds Ratio
-#' @description Produces (symmetric, normality based) confidence bounds
-#' for the ROR, for a passed probability. If called twice with adequate
-#' parameters, these bounds create a confidence interval.
+#' @description Mainly for use in \code{\link{ror}}. Produces (symmetric,
+#' normality based) confidence bounds for the ROR, for a passed probability.
+#' Called twice in \code{ror} to create a confidence interval.
 #' @param sign_lvl_probs The probabilities of the normal distribution, based on
 #' a passed significance level (\code{sign_lvl}) in \code{\link{ror}}. If
 #' \code{sgn_lvl = .95} in \code{ror}, quantiles of the normal distribution will
 #' be extracted at \code{sgn_lvl_probs} of 0.025 and 0.975.
-#' @seealso ror, base::qnorm
+#' @seealso \code{\link{ror}}, \code{\link{base::qnorm}}
 #' @inheritParams ror
 #' @export
 ci_for_ror <- function(a, b, c, d, sign_lvl_probs) {
@@ -16,14 +16,14 @@ ci_for_ror <- function(a, b, c, d, sign_lvl_probs) {
 }
 
 #' @title Confidence intervals for Information Component (IC)
-#' @description Produces quantiles of the posterior gamma distribution. If
-#' called twice with adequate parameters, these bounds create a credibility
-#' interval.
+#' @description Mainly used in \code{link{ic}}. Produces quantiles of the
+#' posterior gamma distribution. Called twice in \code{ic} to create a
+#' credibility interval.
 #' @param sign_lvl_probs The probabilities of the posterior, based on
 #' a passed significance level (\code{sign_lvl}) in \code{\link{ic}}. For
 #' instance, if \code{sgn_lvl = .95} in \code{ic} is used, quantiles will be
 #' extracted at \code{sgn_lvl_probs} 0.025 and 0.975.
-#' @seealso ic, base::qgamma
+#' @seealso \code{\link{ic}}, \code{\link{base::qgamma}}
 #' @inheritParams ic
 #' @export
 ci_for_ic <- function(obs, exp, sign_lvl_probs, shrinkage) {
@@ -40,7 +40,8 @@ ci_for_ic <- function(obs, exp, sign_lvl_probs, shrinkage) {
 #'
 #' @details The ROR is an odds ratio calculated from reporting counts. The
 #' R for Reporting in ROR is meant to emphasize an interpretation of reporting,
-#' as the ROR is calculated from a reporting database.
+#' as the ROR is calculated from a reporting database. Note: the function is
+#' vectorized, i.e. a, b, c and d can be vectors, see the examples.
 #'
 #' @param a Number of reports for the specific drug and event (i.e. the
 #' observed count).
@@ -49,8 +50,8 @@ ci_for_ic <- function(obs, exp, sign_lvl_probs, shrinkage) {
 #' @param d Number of reports without the drug, without the event
 #' @param sign_lvl Significance level of confidence interval. Default is
 #' 0.95 (i.e. 95 \% confidence interval)
-#' @return A tibble with three columns (point estimate and confidence bounds)
-#' with each row for each observed-to-expected pair provided.
+#' @return A tibble with three columns (point estimate and credibility bounds).
+#' Number of rows equals length of inputs a, b, c, d.
 #'
 #' @examples
 #'
@@ -88,7 +89,7 @@ ror <- function(a, b, c, d, sign_lvl = 0.95){
 #' @description Calculates the information component ("IC") and credibility
 #' interval, used in disproportionality analysis.
 #'
-#' @details The IC is based on the relative reporting rate (RRR), but modified with
+#' @details  The IC is based on the relative reporting rate (RRR), but modified with
 #' an addition of "shrinkage" (typically of 0.5) to protect against spurious
 #' associations.
 #'
@@ -107,6 +108,8 @@ ror <- function(a, b, c, d, sign_lvl = 0.95){
 #' observed and expected equal to 0.5. The point of \eqn{log_{2}} is to provide
 #' a log-scale for convenient plotting of multiple IC values side-by-side.
 #'
+#' Note: the function is vectorized, i.e. obs and exp can be vectors, see the examples.
+#'
 #' @param obs A numeric vector with observed counts, i.e. number of reports
 #' for the selected drug-event-combination.
 #' @param exp A numeric vector with expected counts, i.e. number of reports
@@ -116,7 +119,7 @@ ror <- function(a, b, c, d, sign_lvl = 0.95){
 #' @param sign_lvl Significance level of credibility interval. Default is
 #' 0.95 (i.e. 95 \% credibility interval)
 #'
-#' @return A tibble with a row for each observed-to-expected pair provided.
+#' @return A tibble with three columns (point estimate and credibility bounds).
 #'
 #' @examples
 #' pvutils::ic(obs = 20, exp = 10)
